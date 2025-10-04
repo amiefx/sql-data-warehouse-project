6e0a1a9767d286433feb7ee5bdc2836baaf3c14c
amiefx <39983165+amiefx@users.noreply.github.com>
2025-10-04 21:56:10 +0500

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -33,7 +33,7 @@ DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @b
 		PRINT '>> Truncating Table: bronze.crm_cust_info';
 		TRUNCATE TABLE bronze.crm_cust_info;
 		BULK INSERT bronze.crm_cust_info
-		FROM 'C:\Users\mohda\Downloads\Python01\sql-datawarehouse-project\datasets\source_crm\cust_info.csv'
+		FROM 'C:\sql-datawarehouse-project\datasets\source_crm\cust_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -47,7 +47,7 @@ DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @b
 		PRINT '>> Truncating Table: bronze.crm_prd_info';
 		TRUNCATE TABLE bronze.crm_prd_info;
 		BULK INSERT bronze.crm_prd_info
-		FROM 'C:\Users\mohda\Downloads\Python01\sql-datawarehouse-project\datasets\source_crm\prd_info.csv'
+		FROM 'C:\sql-datawarehouse-project\datasets\source_crm\prd_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -61,7 +61,7 @@ DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @b
 		PRINT '>> Truncating Table: bronze.crm_sales_details';
 		TRUNCATE TABLE bronze.crm_sales_details;
 		BULK INSERT bronze.crm_sales_details
-		FROM 'C:\Users\mohda\Downloads\Python01\sql-datawarehouse-project\datasets\source_crm\sales_details.csv'
+		FROM 'C:\sql-datawarehouse-project\datasets\source_crm\sales_details.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -80,7 +80,7 @@ DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @b
 		PRINT '>> Truncating Table: bronze.crm_sales_details';
 		TRUNCATE TABLE bronze.erp_cust_az12;
 		BULK INSERT bronze.erp_cust_az12
-		FROM 'C:\Users\mohda\Downloads\Python01\sql-datawarehouse-project\datasets\source_erp\cust_az12.csv'
+		FROM 'C:\sql-datawarehouse-project\datasets\source_erp\cust_az12.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -94,7 +94,7 @@ DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @b
 		PRINT '>> Truncating Table: bronze.crm_sales_details';
 		TRUNCATE TABLE bronze.erp_loc_a101;
 		BULK INSERT bronze.erp_loc_a101
-		FROM 'C:\Users\mohda\Downloads\Python01\sql-datawarehouse-project\datasets\source_erp\loc_a101.csv'
+		FROM 'C:\sql-datawarehouse-project\datasets\source_erp\loc_a101.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -108,7 +108,7 @@ DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @b
 		PRINT '>> Truncating Table: bronze.crm_sales_details';
 		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'C:\Users\mohda\Downloads\Python01\sql-datawarehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+		FROM 'C:\sql-datawarehouse-project\datasets\source_erp\px_cat_g1v2.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
